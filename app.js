cb357b274d6a2433e73ab8f7409f05993a0ba798
Shyamal Joshi <shyamaljoshi2003@gmail.com>
2023-07-02 12:42:35 +0530

    Added custom error handler functions and some refactor of code

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./connection/db');
+const notFound = require('./middleware/not-fount');
+const errorHandler = require('./middleware/error-handler');
 require('dotenv').config();
 
 app.use(express.json())
 
 app.use('/api/v1/tasks',tasks);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 
